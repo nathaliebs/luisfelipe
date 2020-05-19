0a3b0c3d50ebaddfd0d2e2391dc690596acb55c6
nathaliebs <nathalie.brito@avante.com.vc>
2020-05-18 23:00:10 -0300

    adding a simple menu to route the views

--- src/Views/Index.js
+++ src/Views/Index.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import AboutLuis from './AboutLuis'
+import LuisCoin from './LuisCoin'
+import LuisCountries from './LuisCountries'
 
 function Index(){
-    return <h1>Index</h1>
+    return (
+        <>
+            <BrowserRouter>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/about">About Luis</Link>
+                        </li>
+                        <li>
+                            <Link to="/luiscoin">LuisCoin</Link>
+                        </li>
+                        <li>
+                            <Link to="/countries">Luis Countries</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Route path="/about" component={ AboutLuis } />
+                <Route path="/luiscoin" component={ LuisCoin } />
+                <Route path="/countries" component={ LuisCountries } />
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default Index
