d7c97148f5136f35dbb8f4550d184c2f3bb2ae5b
nathaliebs <nathalie.brito@avante.com.vc>
2020-05-19 21:24:25 -0300

    creating Lifes Luis Changed view and adding to the Index menu

--- src/Views/Index.js
+++ src/Views/Index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom'
 import AboutLuis from './AboutLuis'
 import LuisCoin from './LuisCoin'
 import LuisCountries from './LuisCountries'
+import ChangedLifes from './LifesLuisChanged'
 
 function Index(){
     return (
@@ -19,12 +20,16 @@ function Index(){
                         <li>
                             <Link to="/countries">Luis Countries</Link>
                         </li>
+                        <li>
+                            <Link to="/changed-lifes">Lifes Luis Changed</Link>
+                        </li>
                     </ul>
                 </nav>
 
                 <Route path="/about" component={ AboutLuis } />
                 <Route path="/luiscoin" component={ LuisCoin } />
                 <Route path="/countries" component={ LuisCountries } />
+                <Route path="/changed-lifes" component={ ChangedLifes } />
             </BrowserRouter>
         </>
     )
---
+++ src/Views/LifesLuisChanged.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ChangedLifes(){
+    return <h1>Changed Lifes</h1>
+}
+
+export default ChangedLifes
