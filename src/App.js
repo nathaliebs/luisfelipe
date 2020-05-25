bec32f23b5a434021ff7ce1a958b85bfbd2be445
nathaliebs <nathalie.brito@avante.com.vc>
2020-05-24 21:34:49 -0300

    fix conflits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
-import SideMenu from './Components/SideMenu';
+import React from 'react'
+import './App.css'
+import SideMenu from './Components/SideMenu'
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
-    <SideMenu />
+    <>
+      <NavBar />
+      <SideMenu />
+    </>
   );
 }
 
