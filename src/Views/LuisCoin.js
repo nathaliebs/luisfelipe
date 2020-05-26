ef7b232a70dceb33d894b26cecff2b16aea7d032
Arthur Freitas <arthur.abbrr@gmail.com>
2020-05-26 20:19:54 -0300

    Page Luis Coin

--- src/Views/LuisCoin.js
+++ src/Views/LuisCoin.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import '../Styles/luis-coin.css'
+
 function LuisCoin(){
-    return <h1>LuisCoin</h1>
+    return (
+        <div className="lcoin-content">
+            <p className="title">Um <strong>L</strong>coin (L$ 1) é igual a</p>
+            <p className="dolars">US$ 423.354.171.234.189,98</p>
+            <p className="alert">(dólares americanos)</p>
+        </div>
+    )
 }
 
 export default LuisCoin
