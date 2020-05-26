a96f85fe5f4672389273d55f7222adaab1916c86
Arthur Freitas <arthur.abbrr@gmail.com>
2020-05-26 02:42:54 -0300

    aplying header stylesheet with CSS Grids

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,41 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Link } from 'react-router-dom'
-import { makeStyles } from '@material-ui/core'
 
-const useStyles = makeStyles({
-    nav: {
-        backgroundColor: 'transparent',
-        height: '72px',
-        position: 'fixed',
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'flex-end',
-    },
-    link: {
-        marginTop: '22px', // valor do marginTop no figma (62px) - marginTop global(40px)
-        fontSize: '16px',
-        paddingRight: '20px',
-        textDecoration: 'none',
-        color: '#4D4D4D',
-    },
-    logo: {
-        position: 'absolute',
-        left: '0',
-        color: '#4D4D4D',
-    }
-})
+import '../Styles/header.css'
+import Logo from '../logo.svg'
 
 function Header() {
-    const classes = useStyles()
-
     return (
         <>
         <BrowserRouter>
-            <div className={classes.nav}>
-                <h2 className={classes.logo}>Fãs do Luis</h2>
-                <Link to="/" className={classes.link}>Explicando o site</Link>
-                <Link to="/" className={classes.link}>Quem somos?</Link>
-                <Link to="/" className={classes.link}>Contato</Link>
+            <div className="header">
+                <img src={Logo} alt="Logo" />
+                <nav>
+                    <Link to="/">Explicando o site</Link>
+                    <Link to="/">Quem somos?</Link>
+                    <Link to="/">Contato</Link>
+                </nav>
             </div>
         </BrowserRouter>
         </>
